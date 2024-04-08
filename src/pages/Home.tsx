59d09c61d2bddd6ba80abe7285cbb8bbdfaafac7
Vinayak S <vinayaksukhalal@gmail.com>
2024-04-08 13:00:20 +0530

    inicialised

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,8 @@ import React from 'react'
 import "../css/home.css"
 function Home() {
   return (
- <div className=' bg-red-500 h-screen'>
-    <div style={{backgroundColor:"red",display:"flex",gap:"10rem",height:"3rem", paddingLeft:"500px",paddingTop:"10px"}}>
-        <h1>Home</h1>
-        <h1>History </h1>
-        <h1>Activities</h1>
-        <h1>Gallery</h1>
-    </div>
-
- </div>
+ <>
+ </>
   )
 }
 
