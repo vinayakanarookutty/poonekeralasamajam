1cb5d9fa712f08297b16dd2dcfd2e44136af0893
Vinayak S <vinayaksukhalal@gmail.com>
2024-04-08 11:20:25 +0530

    Project Initialised

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="h-screen">
+   
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/assets/img/pks_bck.jpg')",
+     
+      }
+    },
+  },
+  plugins: [],
+}
