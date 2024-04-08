673f5bfa318e9952054023840c20d11323489d43
Ajith PM <ajithpmdwaraka@gmail.com>
2024-04-08 12:56:29 +0530

    UI added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,45 @@ import React from 'react'
 import "../css/home.css"
 function Home() {
   return (
- <div className=' bg-red-500 h-screen'>
-    <div style={{backgroundColor:"red",display:"flex",gap:"10rem",height:"3rem", paddingLeft:"500px",paddingTop:"10px"}}>
-        <h1>Home</h1>
-        <h1>History </h1>
-        <h1>Activities</h1>
-        <h1>Gallery</h1>
-    </div>
+    <>
+  <header id="header" className="fixed-top header-transparent">  
+    <div className="container flex align-items-center justify-content-between position-relative">
 
- </div>
-  )
+      <div className="logo">
+        <h1 className="text-light"><a href="index.html">Poona Keraleeya Samaj</a></h1>
+        <a href="index.html">
+          <img src="/assets/img/pks_logo.jpg" alt="" className="img-fluid"/></a>
+      </div>
+
+      <nav id="navbar" className="navbar">
+        <ul>
+          <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a className="nav-link scrollto" href="#about">History</a></li>
+          <li><a className="nav-link scrollto" href="#services">Activities</a></li>
+          <li><a className="nav-link scrollto" href="#portfolio">Gallery</a></li>
+          <li><a className="nav-link scrollto" href="#testimonials">PKS Committee</a></li>
+          <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i className="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+    </div>
+  </header> 
+  <section id="hero">
+    <div className="hero-container" data-aos="fade-up">
+      <h1>Welcome to PKS</h1>
+      <h2>Where Pune Meets Kerala : Poona Keraleeya Samaj </h2>
+      <a href="#about" className="btn-get-started scrollto"><i className="bx bx-chevrons-down"></i></a>
+    </div>
+  </section>
+  </> 
+
+
+
+ 
+
+
+  );
 }
 
 export default Home
