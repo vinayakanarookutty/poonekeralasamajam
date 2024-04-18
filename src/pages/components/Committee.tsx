80e1aedf404035da495024aa22b7e5bd64fdf426
Vinayak S <vinayaksukhalal@gmail.com>
2024-04-18 12:48:22 +0530

    Updated

--- src/pages/components/Committee.tsx
+++ src/pages/components/Committee.tsx
@@ -1,42 +1,42 @@
-import React from 'react';
-
+import React from 'react'
+import "../../css/home.css"
 
 function Testimonials() {
   return (
-    <section id="testimonials" className="testimonials section-bg">
+    <section id="testimonials" className="testimonials section-bg mt-5 h-screen">
       <div className="container">
         <div className="section-title" data-aos="fade-in" data-aos-delay="100">
           <h2>Present Managing Committee</h2>
           <p>The current PKS Managing Committee, led by Shri. B. Madhusudan Nair, comprises dedicated middle-aged and young members. They are actively sustaining the society's activities on a large scale. In the late 1970s, PKS had just 8 Life Members and fewer than 100 ordinary members, with fees of Rs.3/- for ordinary members and Rs.25/- for Life Members annually. Despite the Life Membership Fee now being Rs.1000/-, PKS has grown its Life Membership to 350, showcasing a commendable achievement in expanding its membership base.</p>
         </div>
 
-        <div className="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
-          <div className="swiper-wrapper">
-            <div className="swiper-slide">
-              <div className="testimonial-item">
-                <img src="src/assets/img/committee/committee/Bmadhunair.jpg" className="testimonial-img" alt="" />
-                <h3>B Madhu Nair</h3>
-                <h4>President &amp; 9822061992</h4>
+        <div className='flex-row justify-center gap-4'>
+          <div className="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+            <div className="swiper-wrapper flex items-center gap-14">
+              <div className="swiper-slide">
+                <div className="testimonial-item">
+                  <img src="src/assets/img/committee/committee/Bmadhunair.jpg" className="testimonial-img" alt="" />
+                  <h3>B Madhu Nair</h3>
+                  <h4>President &amp; 9822061992</h4>
+                </div>
               </div>
-            </div>
 
-            <div className="swiper-slide">
-              <div className="testimonial-item">
-                <img src="src/assets/img/committee/committee/vjy.jpg" className="testimonial-img" alt="" />
-                <h3>K Vijayakumar</h3>
-                <h4>Vice President &amp; 9373318875</h4>
+              <div className="swiper-slide">
+                <div className="testimonial-item">
+                  <img src="src/assets/img/committee/committee/vjy.jpg" className="testimonial-img" alt="" />
+                  <h3>K Vijayakumar</h3>
+                  <h4>Vice President &amp; 9373318875</h4>
+                </div>
               </div>
-            </div>
 
-            <div className="swiper-slide">
-              <div className="testimonial-item">
-                <img src="src/assets/img/committee/committee/Gopall.jpg" className="testimonial-img" alt="" />
-                <h3>Gopalakrishnan Nair.D</h3>
-                <h4>General Secretary &amp; 9822271700 </h4>
+              <div className="swiper-slide">
+                <div className="testimonial-item">
+                  <img src="src/assets/img/committee/committee/Gopall.jpg" className="testimonial-img" alt="" />
+                  <h3>Gopalakrishnan Nair.D</h3>
+                  <h4>General Secretary &amp; 9822271700 </h4>
+                </div>
               </div>
-            </div>
-
-            {/* <div className="swiper-slide">
+             {/* <div className="swiper-slide">
               <div className="testimonial-item">
                 <img src="/src/assets/img/committee/MN_Nambiar.jpg" className="testimonial-img" alt="" />
                 <h3>M. N. Nambiar</h3>
@@ -67,8 +67,9 @@ function Testimonials() {
                 <h4>Joint Secretary &amp; 9850972250</h4>
               </div>
             </div>
+            </div>
+            <div className="swiper-pagination"></div>
           </div>
-          <div className="swiper-pagination"></div>
         </div>
       </div>
     </section>
