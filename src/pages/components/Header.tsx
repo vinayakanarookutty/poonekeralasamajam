f67c1b1f96c9d1074085f78678fc74bea6a829b1
Vinayak S <vinayaksukhalal@gmail.com>
2024-04-18 10:23:21 +0530

    css added

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,31 +1,50 @@
-import React from 'react';
-import "../../css/home.css"
+import React, { useState, useEffect } from 'react';
+import "../../css/home.css";
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrolled = window.scrollY > 0;
+      setIsScrolled(scrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-    <header data-aos="fade-up" id="header" className="fixed-top header-transparent mt-2 ">
-      <div className="container flex align-items-center justify-content-between position-relative ">
-        <div className="logo flex gap-4">
-         
-          <a >
-            <img src="/src/assets/img/pks_logo.png" alt="" className="img-fluid" />
-          </a>
-          <h1 className="text-light ml-5 mt-2  ">Poona Keraleeya Samaj</h1>
+      <div
+        data-aos="fade-up"
+        id="header"
+        style={{height:"70px"}}
+        className={`fixed-top ${isScrolled ? 'header-scrolled' : 'header-transparent'}`}
+      >
+        <div className="container flex align-items-center justify-content-between position-relative ">
+          <div className="logo flex gap-4">
+            <a>
+              <img src="/src/assets/img/pks_logo.png" alt="" className="img-fluid" />
+            </a>
+            <h1 className="text-light ml-5 mt-2  ">Poona Keraleeya Samaj</h1>
+          </div>
+          <nav id="navbar" className="navbar">
+            <ul>
+              <li><a className="nav-link scrollto active text-white" href="#hero">Home</a></li>
+              <li><a className="nav-link scrollto  text-white" href="#about">History</a></li>
+              <li><a className="nav-link scrollto  text-white" href="#services">Activities</a></li>
+              <li><a className="nav-link scrollto  text-white" href="#portfolio">Gallery</a></li>
+              <li><a className="nav-link scrollto  text-white" href="#testimonials">PKS Committee</a></li>
+              <li><a className="nav-link scrollto  text-white" href="#contact">Contact</a></li>
+            </ul>
+            <i className="bi bi-list mobile-nav-toggle"></i>
+          </nav>
         </div>
-        <nav id="navbar" className="navbar">
-          <ul>
-            <li><a className="nav-link scrollto active text-white" href="#hero">Home</a></li>
-            <li><a className="nav-link scrollto  text-white" href="#about">History</a></li>
-            <li><a className="nav-link scrollto  text-white" href="#services">Activities</a></li>
-            <li><a className="nav-link scrollto  text-white" href="#portfolio">Gallery</a></li>
-            <li><a className="nav-link scrollto  text-white" href="#testimonials">PKS Committee</a></li>
-            <li><a className="nav-link scrollto  text-white" href="#contact">Contact</a></li>
-          </ul>
-          <i className="bi bi-list mobile-nav-toggle"></i>
-        </nav>
       </div>
-    </header>
     </>
   );
 }
