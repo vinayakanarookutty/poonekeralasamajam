7df2d926b9453134f712d189573a18e2ab4bdccf
Vinayak S <vinayaksukhalal@gmail.com>
2024-05-09 23:24:46 +0530

    Updated

--- src/pages/components/Cmembers.tsx
+++ src/pages/components/Cmembers.tsx
@@ -3,13 +3,109 @@ import "./../../css/home.css"
 
 function Team() {
   return (
-    <section id="team" className="team h-screen">
-      <div className="container">
+    <section id="team" className="team h-screen ">
+      <div className="container ">
         <div className="section-title" data-aos="fade-in" data-aos-delay="100">
-          <h2>Committee Members</h2>
+        <h2>Present Managing Committee</h2>
+        <p>The current PKS Managing Committee, led by Shri. B. Madhusudan Nair, comprises dedicated middle-aged and young members. They are actively sustaining the society's activities on a large scale. In the late 1970s, PKS had just 8 Life Members and fewer than 100 ordinary members, with fees of Rs.3/- for ordinary members and Rs.25/- for Life Members annually. Despite the Life Membership Fee now being Rs.1000/-, PKS has grown its Life Membership to 350, showcasing a commendable achievement in expanding its membership base.</p>
         </div>
-        <div className="row">
-          <div className="col-lg-4 col-md-6">
+        <div className="flex overflow-auto scroll-m-7 scroll-p-7" >
+          <div className="col-lg-3 col-md-6">
+            <div className="member" data-aos="fade-up">
+              <div className="pic"> <img src="src/assets/img/committee/committee/Bmadhunair.jpg" className="img-fluid" alt=""/> </div>
+              <div className="member-info">
+                <h4>Anil George</h4>
+                <span>President</span>
+                <div className="social">
+                  <a href=""><i className="bi bi-twitter"></i></a>
+                  <a href=""><i className="bi bi-facebook"></i></a>
+                  <a href=""><i className="bi bi-instagram"></i></a>
+                  <a href=""><i className="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6">
+            <div className="member" data-aos="fade-up">
+              <div className="pic"> <img src="src/assets/img/committee/committee/vjy.jpg" className="img-fluid" alt=""/> </div>
+              <div className="member-info">
+                <h4>K Vijayakumar</h4>
+                <span>Vice President</span>
+                <div className="social">
+                  <a href=""><i className="bi bi-twitter"></i></a>
+                  <a href=""><i className="bi bi-facebook"></i></a>
+                  <a href=""><i className="bi bi-instagram"></i></a>
+                  <a href=""><i className="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6">
+            <div className="member" data-aos="fade-up">
+              <div className="pic"> <img src="src/assets/img/committee/committee/Gopall.jpg" className="img-fluid" alt=""/> </div>
+              <div className="member-info">
+                <h4>Gopalakrishnan Nair.D</h4>
+                <span>General Secretary</span>
+                <div className="social">
+                  <a href=""><i className="bi bi-twitter"></i></a>
+                  <a href=""><i className="bi bi-facebook"></i></a>
+                  <a href=""><i className="bi bi-instagram"></i></a>
+                  <a href=""><i className="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6">
+            <div className="member" data-aos="fade-up">
+              <div className="pic"> <img src="src/assets/img/committee/committee/Jagdish_Poduval.jpg" className="img-fluid" alt=""/> </div>
+              <div className="member-info">
+                <h4>Jagdish Poduval</h4>
+                <span>Joint Treasurer</span>
+                <div className="social">
+                  <a href=""><i className="bi bi-twitter"></i></a>
+                  <a href=""><i className="bi bi-facebook"></i></a>
+                  <a href=""><i className="bi bi-instagram"></i></a>
+                  <a href=""><i className="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6">
+            <div className="member" data-aos="fade-up">
+              <div className="pic"> <img src="src/assets/img/committee/committee/sureshn.jpg" className="img-fluid" alt=""/> </div>
+              <div className="member-info">
+                <h4>C. Sureshan</h4>
+                <span>Joint Treasurer</span>
+                <div className="social">
+                  <a href=""><i className="bi bi-twitter"></i></a>
+                  <a href=""><i className="bi bi-facebook"></i></a>
+                  <a href=""><i className="bi bi-instagram"></i></a>
+                  <a href=""><i className="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <div className="member" data-aos="fade-up">
+              <div className="pic"> <img src="src/assets/img/committee/committee/som.jpg" className="img-fluid" alt=""/> </div>
+              <div className="member-info">
+                <h4>C. Soman</h4>
+                <span>Joint Treasurer</span>
+                <div className="social">
+                  <a href=""><i className="bi bi-twitter"></i></a>
+                  <a href=""><i className="bi bi-facebook"></i></a>
+                  <a href=""><i className="bi bi-instagram"></i></a>
+                  <a href=""><i className="bi bi-linkedin"></i></a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6">
             <div className="member" data-aos="fade-up">
               <div className="pic"> <img src="/src/assets/img/c_members/Anil-George.jpg" className="img-fluid" alt=""/> </div>
               <div className="member-info">
@@ -25,8 +121,8 @@ function Team() {
             </div>
           </div>
 
-          <div className="col-lg-4 col-md-6">
-            <div className="member" data-aos="fade-up" data-aos-delay="150">
+          <div className="col-lg-3 col-md-6">
+            <div className="member" data-aos="fade-up" >
               <div className="pic"><img src="/src/assets/img/c_members/vijayamnair-2.jpg" className="img-fluid" alt=""/></div>
               <div className="member-info">
                 <h4>Vijayam Nair</h4>
@@ -41,21 +137,6 @@ function Team() {
             </div>
           </div>
 
-          <div className="col-lg-4 col-md-6">
-            <div className="member" data-aos="fade-up" data-aos-delay="300">
-              <div className="pic"><img src="/src/assets/img/c_members/sarasamma-1.jpg" className="img-fluid" alt=""/></div>
-              <div className="member-info">
-                <h4>Sarasamma</h4>
-                <span>Cultural Committee Convener</span>
-                <div className="social">
-                  <a href=""><i className="bi bi-twitter"></i></a>
-                  <a href=""><i className="bi bi-facebook"></i></a>
-                  <a href=""><i className="bi bi-instagram"></i></a>
-                  <a href=""><i className="bi bi-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </section>
--- src/pages/components/History.tsx
+++ src/pages/components/History.tsx
@@ -1,19 +1,25 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Image from "../../assets/img/pks_act.jpg"
 import React from 'react';
 
 const About = () => {
     return (
-      <section>
+        
+      <section className='h-screen'>
+        <div className='h-screen'>
+        <div className="section-title mt-[100px]" data-aos="fade-in" data-aos-delay="100">
+          <h2>The History Of Poona Keraleeya Samaj</h2>
+        </div>
+        <ol className="items-center  sm:flex h-[400px]   ">
       
-        <ol className="items-center sm:flex">
             <li className="relative mb-6 sm:mb-0">
                 <div className="flex items-center">
-                    <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                        <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <div className="z-10 flex items-center justify-center w-[300px] h-22 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                        {/* <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                        </svg>
+                        </svg> */}
+                         <img src={Image} className="img-fluid"  />
                     </div>
                     <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                 </div>
@@ -54,7 +60,7 @@ const About = () => {
                 </div>
             </li>
         </ol>
-
+        </div>
       </section>  
     );
 }
