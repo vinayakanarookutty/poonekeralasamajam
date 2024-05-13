2cb3234c00d0c04de8119c7108be266fb7c79103
ab-h-i-n <abhinshelly@gmail.com>
2024-05-13 11:20:48 +0530

    timeline and members card updated - added responsive

--- src/pages/components/PCMemberCard.tsx
+++ src/pages/components/PCMemberCard.tsx
@@ -10,11 +10,11 @@ interface PCMemberCardProps {
 const PCMemberCard: React.FC<PCMemberCardProps> = ({ image, name, position, phone }) => {
   return (
     <div className="grid place-items-center gap-2">
-      <div className="aspect-square w-[200px] overflow-hidden rounded-full">
+      <div className="aspect-square w-[100px] md:w-[150px] lg:w-[200px] overflow-hidden rounded-full">
         <img src={image} alt="" className="object-cover" />
       </div>
-      <p className="font-bold text-xl">{name}</p>
-      <div className="text-slate-500 text-sm">
+      <p className="font-bold text-sm md:text-xl text-center">{name}</p>
+      <div className="text-center text-slate-500 text-sm">
         <p>{position} </p>
         <p> {phone}</p>
       </div>
