bbf58a30f3d6a95abed4bf4ed7d3b8062992331d
Vinayak S <vinayaksukhalal@gmail.com>
2024-05-13 14:34:30 +0530

    Updated

--- src/pages/components/BgCarousel.tsx
+++ src/pages/components/BgCarousel.tsx
@@ -20,9 +20,9 @@ const BgCarousel: React.FC = () => {
         >
            {images.map((image, index) => (
     <SwiperSlide key={index}>
-        <div className="w-screen overflow-hidden max-h-screen">
+        <div className=" h-screen  w-screen overflow-hidden max-h-screen ">
             <div className="h-full w-full bg-[#378db540] absolute top-0 left-0 z-50"></div>
-            <img src={image} alt={`img_${index}`} className="h-screen md:w-screen md:h-auto object-cover brightness-50" />
+            <img src={image} alt={`img_${index}`} className="h-screen md:w-screen md:h-screen object-cover brightness-50" />
         </div>
     </SwiperSlide>
 ))}
