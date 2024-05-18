607b8e7067befcaee4db45410483f5be9ed988d4
Ajith PM <ajithpmdwaraka@gmail.com>
2024-05-18 11:17:01 +0530

    implemented router in activity component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import ErrorPage from "./pages/ErrorPage";
-import Home from "./pages/Home";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import MoreActivities from './pages/components/MoreActivities';
 
 function App() {
-  
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: <Home />,
-      errorElement: <ErrorPage/>,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/more',
+      element: <MoreActivities />,
     },
   ]);
+
   return (
-    <>
-   <RouterProvider router={router} />
-    </>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
-export default App
+export default App;
