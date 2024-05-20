5e40332161d6b3e7764f5c5ddeb6ab1f96da7b92
Vinayak S <vinayaksukhalal@gmail.com>
2024-05-20 18:57:29 +0530

    Updated

--- src/pages/components/MoreGallery.tsx
+++ src/pages/components/MoreGallery.tsx
@@ -5,7 +5,10 @@ import clb2Image from '../../assets/img/gallery/clb2.jpg';
 import clb3Image from '../../assets/img/gallery/clb3.jpg';
 import onamImage from '../../assets/img/gallery/onam.jpg';
 import prize_disImage from '../../assets/img/gallery/prize_dis.jpg';
-
+import { Breadcrumb } from "flowbite-react";
+import { HiHome } from "react-icons/hi";
+import { Accordion } from "flowbite-react";
+import { ListGroup } from "flowbite-react";
 function MoreGallery() {
   const data = [
     {
@@ -51,12 +54,22 @@ function MoreGallery() {
 
   return (
     <>
+    <div className='absolute mt-5 ml-10'>
+      <Breadcrumb aria-label="Default breadcrumb example">
+      <Breadcrumb.Item href="#" icon={HiHome}>
+        Home
+      </Breadcrumb.Item>
+      <Breadcrumb.Item href="#">Gallery</Breadcrumb.Item>
+      </Breadcrumb>
+    </div>
       <div className="grid  py-10 min-h-screen place-items-center w-screen overflow-x-hidden relative">
         {/* header  */}
+    
         <div className="section-title grid justify-center">
           {/* title  */}
           <h2 className="text-2xl font-bold">Gallery</h2>
           {/* search  */}
+    
           <form className=' w-[350px] focus-within:ring-2 ring-sky-400 h-min bg-slate-300 grid grid-cols-[1fr,1rem] rounded-full pr-5'>
               {/* serch input  */}
               <input className="focus:outline-none bg-transparent w-full py-3 pr-3 pl-5 text-sm" placeholder="Search Mockups, Logos..." required />
@@ -69,6 +82,7 @@ function MoreGallery() {
                 </div>
               </button>
           </form>
+        
         </div>
 
         {/* active image  */}
@@ -93,7 +107,10 @@ function MoreGallery() {
             </div>
           ))}
         </div>
-      </div>
+        
+</div>        
+
+    
     </>
   );
 }
