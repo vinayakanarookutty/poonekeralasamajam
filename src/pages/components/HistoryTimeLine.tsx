8f1df22f76a29445ad31cf1a7ffeb62cf3c866bb
Suhana-DBC <suhuscornerzz@gmail.com>
2024-05-22 18:50:17 +0530

    first commit

--- src/pages/components/HistoryTimeLine.tsx
+++ src/pages/components/HistoryTimeLine.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import "../../css/style.css";
+import "../../css/home.css";
+import timelineImage from '../../assets/img/amazon.png';
+import timelineImage2 from '../../assets/img/google.jpg';
 
 function HistoryTimeLine() {
   
   return (
     <div className= "history_timeline">
     <div className="timeline_container left-container">
-    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSalcmcanaYko-B2I70L-qa22UnJxVA4PdL_1QfBxQkIg&s' alt='google'></img>
+    <img src={timelineImage2} alt='google'></img>
       <div className="text-box">
         <h2> Alphabet Inc.</h2>
         <small>2018-2019</small>
@@ -15,7 +17,7 @@ function HistoryTimeLine() {
       </div>
     </div>
     <div className="timeline_container right-container">
-    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsXT8zCWw30eTRGLF_N1cGtXOvTdHhH9JRYK5rjVnWsA&s' alt='amazon'></img>
+    <img src={timelineImage} alt='amazon'></img>
       <div className="text-box">
         <h2> Amazon Com.</h2>
         <small>2018-2019</small>
@@ -24,7 +26,7 @@ function HistoryTimeLine() {
       </div>
     </div>
     <div className="timeline_container left-container">
-    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSalcmcanaYko-B2I70L-qa22UnJxVA4PdL_1QfBxQkIg&s' alt='google'></img>
+    <img src={timelineImage2} alt='google'></img>
       <div className="text-box">
         <h2> Alphabet Inc.</h2>
         <small>2018-2019</small>
@@ -32,9 +34,28 @@ function HistoryTimeLine() {
         <span className='left-container-arrow'></span>
       </div>
     </div>
+    
     <div className="timeline_container right-container">
-    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsXT8zCWw30eTRGLF_N1cGtXOvTdHhH9JRYK5rjVnWsA&s' alt='amazon'></img>
-
+    <img src={timelineImage} alt='google'></img>
+      <div className="text-box">
+        <h2> Amazon Inc.</h2>
+        <small>2018-2019</small>
+        <p>The success of every websites depends on search engine optimisation and digital </p>
+        <span className='right-container-arrow'></span>
+      </div>
+    </div>
+    <div className="timeline_container left-container">
+    <img src={timelineImage2} alt='google'></img>
+      <div className="text-box">
+        <h2> Alphabet Inc.</h2>
+        <small>2018-2019</small>
+        <p>The success of every websites depends on search engine optimisation and digital </p>
+        <span className='left-container-arrow'></span>
+      </div>
+    </div>
+    
+    <div className="timeline_container right-container">
+    <img src={timelineImage} alt='google'></img>
       <div className="text-box">
         <h2> Amazon Inc.</h2>
         <small>2018-2019</small>
@@ -43,6 +64,7 @@ function HistoryTimeLine() {
       </div>
     </div>
    </div>
+   
   )
 }
 
