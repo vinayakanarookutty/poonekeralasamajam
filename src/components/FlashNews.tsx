996f53f4a9d86f9011f6c1513bea562cc3363498
Sreenandha V R <sreenandhavr161@gmail.com>
2024-05-24 11:27:31 +0530

    Flash news Added

---
+++ src/components/FlashNews.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Marquee from './Marquee';
+
+import "../../css/home.css";
+
+const FlashNews: React.FC = () => {
+    return (
+        <div className="flash-news">
+            <div className="row">
+                <div className="truncate">
+                    <center>
+                        <p className="contentfont">FLASH NEWS<i className="material-icons right"></i></p>
+                    </center>
+                </div>
+                <div className="body">
+                    <Marquee>
+                    </Marquee>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FlashNews;
